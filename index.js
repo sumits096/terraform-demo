f3ac9280a87e257138079421a9b45e1c1c56c63b
Sumit <sumits@incubxperts.com>
2021-10-18 14:34:21 +0530

    Changes for message

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Yes. CICD testing with Terraform..........';
+  const name = process.env.NAME || 'CICD testing with Terraform..........';
   res.send(`Test Changes ${name}!`);
 });
 
