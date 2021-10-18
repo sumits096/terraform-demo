b72ffe5ff6b5a42968937dfc8e1753d8c506c50d
Sumit <sumits@incubxperts.com>
2021-10-18 17:10:29 +0530

    Configuration changes

--- main.tf
+++ main.tf
@@ -18,13 +18,20 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
-data "terraform_remote_state" "network" {
-  backend = "remote"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
 
-  config = {
-    organization = "IX"
-    workspaces = {
-          name = "terraform-demo"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
     }
   }
 }
