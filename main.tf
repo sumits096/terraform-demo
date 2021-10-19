40d1e5f6788677499a740aa8cb4f818ed0d0731f
Sumit <sumits@incubxperts.com>
2021-10-19 14:19:32 +0530

    Configuration changes

--- main.tf
+++ main.tf
@@ -17,3 +17,32 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_project" "terraform-first-app" {
+  name       = "terraform-first-app"
+  project_id = "terraform-first-app"
+  org_id     = "1001419021452"
+}
+
+resource "google_app_engine_application" "app" {
+  project     = google_project.terraform-first-app.project_id
+  location_id = "us-central"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
