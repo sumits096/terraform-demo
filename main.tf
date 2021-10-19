8b40086252d0fd97d0f27f9c935c1972f17a1955
Sumit <sumits@incubxperts.com>
2021-10-19 17:50:02 +0530

    message changes

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "google_cloud_run_service" "default" {
   template {
     spec {
       containers {
-        image = "us-docker.pkg.dev/cloudrun/container/terraform-first-app"
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
       }
     }
   }
