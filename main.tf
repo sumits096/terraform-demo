a8533ef1e54c6b22336aac8a1051a13c761519e8
Sumit <sumits@incubxperts.com>
2021-10-19 18:28:02 +0530

    Deployment Testing

--- main.tf
+++ main.tf
@@ -42,9 +42,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.terraform-cloud-run.location
-  project     = google_cloud_run_service.terraform-cloud-run.project
-  service     = google_cloud_run_service.terraform-cloud-run.name
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
