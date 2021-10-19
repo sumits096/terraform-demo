b129d8d793f9039594a74e8e6c6f31fce9d05da9
Sumit <sumits@incubxperts.com>
2021-10-19 18:15:35 +0530

    message changes

--- main.tf
+++ main.tf
@@ -26,9 +26,3 @@ resource "google_cloud_run_service" "terraform-cloud-run" {
     }
   }
 }
-
-# Display the service URL
-output "service_url" {
-  value = google_cloud_run_service.run_service.status[0].url
-}
-
