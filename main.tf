78ae368361388fa4ed34f06a532efa59f1055642
Sumit <sumits@incubxperts.com>
2021-10-20 12:45:23 +0530

    Deployment Testing

--- main.tf
+++ main.tf
@@ -9,7 +9,36 @@ terraform {
 
 provider "google" {
   credentials = file("service-account.json")
-  project = "terraform-deployment-demo"
-  region  = "us-central1"
+  project = var.project
+  region  = var.region
   zone    = "us-central1-c"
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
