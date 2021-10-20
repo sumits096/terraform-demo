f8896336c1caef10e53478b57a8ca42639c27c2b
Sumit <sumits@incubxperts.com>
2021-10-20 14:51:14 +0530

    Deployment Testing

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'CICD testing with terraform  2...';
+  const name = process.env.NAME || 'CICD testing with terraform 3...';
   res.send(`Test Changes ${name}!`);
 });
 
