4d47633e95dd77bfb8a0932eb196c4d5d5a698ff
Sumit <sumits@incubxperts.com>
2021-10-20 14:19:28 +0530

    Deployment Testing

--- terraform/modules/function/variables.tf
+++ terraform/modules/function/variables.tf
@@ -1,3 +1,8 @@
-variable "project" {}
-variable "name" {}
+variable "project" {
+    default = "terraform-deployment-demo"
+}
+
+variable "region" {
+    default = "us-central1" # Choose a region
+}
 variable "entry_point" {}
