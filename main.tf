090f081d4fc6c1962e16bdb6b0212e74cb86e6fc
Sumit <sumits@incubxperts.com>
2021-10-21 15:04:19 +0530

    Deployment Testing

--- main.tf
+++ main.tf
@@ -19,57 +19,29 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-# Compress source code
-data "archive_file" "source" {
-  type        = "zip"
-  source_dir  = local.root_dir
-  output_path = "/tmp/function-${local.timestamp}.zip"
-}
-
-# Create bucket that will host the source code
 resource "google_storage_bucket" "bucket" {
-  name = "${var.project}-function"
+  name = "test-bucket"
 }
 
-# Add source code zip to bucket
-resource "google_storage_bucket_object" "zip" {
-  # Append file MD5 to force bucket to be recreated
-  name   = "source.zip#${data.archive_file.source.output_md5}"
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
   bucket = google_storage_bucket.bucket.name
-  source = data.archive_file.source.output_path
+  source = "./path/to/zip/file/which/contains/code"
 }
 
-# Enable Cloud Functions API
-resource "google_project_service" "cf" {
-  project = var.project
-  service = "cloudfunctions.googleapis.com"
-
-  disable_dependent_services = true
-  disable_on_destroy         = false
-}
-
-# Enable Cloud Build API
-resource "google_project_service" "cb" {
-  project = var.project
-  service = "cloudbuild.googleapis.com"
-
-  disable_dependent_services = true
-  disable_on_destroy         = false
-}
-
-# Create Cloud Function
 resource "google_cloudfunctions_function" "function" {
-  name    = "function-test"
-  runtime = "nodejs12"
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs14"
 
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.zip.name
+  source_archive_object = google_storage_bucket_object.archive.name
   trigger_http          = true
-  entry_point           = "helloword"
+  entry_point           = "helloGET"
 }
 
-# Create IAM entry so all users can invoke the function
+# IAM entry for all users to invoke the function
 resource "google_cloudfunctions_function_iam_member" "invoker" {
   project        = google_cloudfunctions_function.function.project
   region         = google_cloudfunctions_function.function.region
