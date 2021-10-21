d26e1467b7fe0bb4aed6afaeed4b3353a0830659
Sumit <sumits@incubxperts.com>
2021-10-21 21:07:26 +0530

    Deployment Testing

--- main.tf
+++ main.tf
@@ -80,3 +80,38 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
   role   = "roles/cloudfunctions.invoker"
   member = "allUsers"
 }
+
+# Deploy image to Cloud Run
+resource "google_cloud_run_service" "mywebapp" {
+  name     = "mywebapp"
+  location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "gcr.io/terraform-cr/webapp"
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Create public access
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+# Enable public access on Cloud Run service
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.mywebapp.location
+  project     = google_cloud_run_service.mywebapp.project
+  service     = google_cloud_run_service.mywebapp.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
