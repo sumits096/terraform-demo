e4b4168b3878d6c9217976bc3fe1032ab9d3011c
Sumit <sumits@incubxperts.com>
2021-10-21 15:19:49 +0530

    Deployment Testing

--- main.tf
+++ main.tf
@@ -19,25 +19,57 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_storage_bucket_object" "archive" {
-  name   = "index.zip"
-  bucket = "terraform-deployment-demo-bucket"
-  source = "./path/to/zip/file/which/contains/code"
+# Compress source code
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = local.root_dir
+  output_path = "/tmp/function-${local.timestamp}.zip"
 }
 
+# Create bucket that will host the source code
+resource "google_storage_bucket" "bucket" {
+  name = "${var.project}-function"
+}
+
+# Add source code zip to bucket
+resource "google_storage_bucket_object" "zip" {
+  # Append file MD5 to force bucket to be recreated
+  name   = "source.zip#${data.archive_file.source.output_md5}"
+  bucket = google_storage_bucket.bucket.name
+  source = data.archive_file.source.output_path
+}
+
+# Enable Cloud Functions API
+resource "google_project_service" "cf" {
+  project = var.project
+  service = "cloudfunctions.googleapis.com"
+
+  disable_dependent_services = true
+  disable_on_destroy         = false
+}
+
+# Enable Cloud Build API
+resource "google_project_service" "cb" {
+  project = var.project
+  service = "cloudbuild.googleapis.com"
+
+  disable_dependent_services = true
+  disable_on_destroy         = false
+}
+
+# Create Cloud Function
 resource "google_cloudfunctions_function" "function" {
-  name        = "function-test"
-  description = "My function"
-  runtime     = "nodejs14"
+  name    = "function-test"
+  runtime = "nodejs12"
 
   available_memory_mb   = 128
-  source_archive_bucket = "terraform-deployment-demo-bucket"
-  source_archive_object = "index.zip"
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.zip.name
   trigger_http          = true
-  entry_point           = "helloGET"
+  entry_point           = "helloword"
 }
 
-# IAM entry for all users to invoke the function
+# Create IAM entry so all users can invoke the function
 resource "google_cloudfunctions_function_iam_member" "invoker" {
   project        = google_cloudfunctions_function.function.project
   region         = google_cloudfunctions_function.function.region
