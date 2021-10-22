65607708f81f353795dcb03322a529772535017f
Sumit <sumits@incubxperts.com>
2021-10-22 15:05:20 +0530

    Deployment Testing

--- main.tf
+++ main.tf
@@ -66,3 +66,37 @@ resource "google_project_service" "run_api" {
   disable_on_destroy = true
 }
 
+# Deploy image to Cloud Run
+resource "google_cloud_run_service" "gcp_cloud_run_service" {
+  name     = "gcp-cloud-run-service"
+  location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Create public access
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+# Enable public access on Cloud Run service
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.gcp_cloud_run_service.location
+  project     = google_cloud_run_service.gcp_cloud_run_service.project
+  service     = google_cloud_run_service.gcp_cloud_run_service.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
