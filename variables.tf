93da4c9f87c31b7eec45f7bac9bd4bc53ee9bf4d
Sumit <sumits@incubxperts.com>
2021-10-22 10:43:33 +0530

    Deployment Testing

--- variables.tf
+++ variables.tf
@@ -5,3 +5,15 @@ variable "project" {
 variable "region" {
     default = "us-central1" # Choose a region
 }
+
+variable "github_repository" {
+    default = "https://github.com/sumitshinde/terraform-demo.git"
+}
+
+variable "github_owner" {
+    default = "sumitshinde096@gmail.com"
+}
+
+variable "github_branch" {
+    default = "^master$"
+}
