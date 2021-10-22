4559f667fd10a4cfc1e6a677e8570fad8b412082
Sumit <sumits@incubxperts.com>
2021-10-22 14:38:07 +0530

    Deployment Testing

--- main.tf
+++ main.tf
@@ -67,8 +67,8 @@ resource "google_project_service" "run_api" {
 }
 
 # Deploy image to Cloud Run
-resource "google_cloud_run_service" "cloud_run_service" {
-  name     = "cloud-run-service"
+resource "google_cloud_run_service" "gcp_cloud_run_service" {
+  name     = "gcp-cloud-run-service"
   location = "us-central1"
   template {
     spec {
@@ -95,8 +95,8 @@ data "google_iam_policy" "noauth" {
 
 # Enable public access on Cloud Run service
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.cloud_run_service.location
-  project     = google_cloud_run_service.cloud_run_service.project
-  service     = google_cloud_run_service.cloud_run_service.name
+  location    = google_cloud_run_service.gcp_cloud_run_service.location
+  project     = google_cloud_run_service.gcp_cloud_run_service.project
+  service     = google_cloud_run_service.gcp_cloud_run_service.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
