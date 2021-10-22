b7efd1182c01704537f9393ba6bcf9ffa1caa714
Sumit <sumits@incubxperts.com>
2021-10-22 10:39:54 +0530

    Deployment Testing

--- main.tf
+++ main.tf
@@ -100,31 +100,3 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   service     = google_cloud_run_service.myterraformwebapp.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
-
-resource "google_cloudbuild_trigger" "service-account-trigger" {
-  trigger_template {
-    branch_name = "master"
-    repo_name   = "https://github.com/sumitshinde/terraform-demo.git"
-  }
-
-  service_account = google_service_account.cloudbuild_service_account.id
-  filename        = "cloudbuild.yaml"
-  depends_on = [
-    google_project_iam_member.act_as,
-    google_project_iam_member.logs_writer
-  ]
-}
-
-resource "google_service_account" "cloudbuild_service_account" {
-  account_id = "my-service-account"
-}
-
-resource "google_project_iam_member" "act_as" {
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
-}
-
-resource "google_project_iam_member" "logs_writer" {
-  role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
-}
